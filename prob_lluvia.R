335cdada023ea5ece73bc5754695a8ef36c6ef78
pabloedugpi <pabloedug@gmail.com>
2016-11-12 10:17:23 +0100

    me llevo prop_lluvia.R a github

---
+++ prob_lluvia.R
@@ -0,0 +1,7 @@
+library(XML)
+fileUrl<-"http://www.aemet.es/xml/municipios/localidad_28080.xml"
+doc <- xmlTreeParse(fileUrl,useInternal=TRUE)
+prob24<-xpathSApply(doc,paste0("//dia[@fecha='",as.character(Sys.Date()+1),"']/prob_precipitacion[@periodo='00-24']"),xmlValue)
+tempMax<-xpathSApply(doc,paste0("//dia[@fecha='",as.character(Sys.Date()+1),"']/temperatura/maxima"),xmlValue)
+if(as.numeric(prob24)>50)(paste0("Coger el paraguas porque lloverá con una probabilidad del ",prob24,"%."))
+if(as.numeric(tempMax)<10)(paste0("Temperatura máxima pronosticada: ",tempMax," grados."))
